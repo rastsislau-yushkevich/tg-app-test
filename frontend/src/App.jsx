ef149cbaf4fb61851fa85b49c8c557e0f7b709f7
Ryu <rastsislauyushkevich@gmail.com>
2024-10-02 14:47:00 +0300

    geo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-	console.log(window.Telegram.WebApp);
-	return <p>ajhsbdjahbsds</p>;
+	const [position, setPosition] = useState('');
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			console.log(position);
+			const { latitude, longitude } = position.coords;
+			setPosition({ latitude, longitude });
+			window.Telegram.WebApp.sendData(JSON.stringify({ latitude, longitude }));
+		});
+	}, []);
+
+	return <p>{JSON.stringify(position)}</p>;
 }
 
 export default App;
