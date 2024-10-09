d4bb3e1e3b6bfc2e38cc35463a2c3745d390605b
Ryu <rastsislauyushkevich@gmail.com>
2024-10-09 11:20:26 +0300

    lib test: failed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,28 +2,9 @@ import { useEffect, useState } from 'react';
 import { YandexMap } from './components/YandexMap';
 
 function App() {
-	const [position, setPosition] = useState('');
-
-	useEffect(() => {
-		navigator.geolocation.getCurrentPosition(
-			(position) => {
-				console.log(position);
-				const { latitude, longitude } = position.coords;
-				setPosition({ latitude, longitude });
-			},
-			(err) => console.log(err)
-		);
-	}, []);
-	console.log(JSON.stringify(position));
 	return (
 		<>
-			<p>{JSON.stringify(position)}</p>
-			{position && (
-				<YandexMap
-					lat={position.latitude}
-					lng={position.longitude}
-				/>
-			)}
+			<YandexMap />
 		</>
 	);
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { YMaps } from '@pbe/react-yandex-maps';
+
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<YMaps>
+			<App />
+		</YMaps>
 	</StrictMode>
 );
