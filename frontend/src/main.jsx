bcf574d25706f80f593a58180aef9570632db983
Ryu <rastsislauyushkevich@gmail.com>
2024-10-09 15:31:57 +0300

    ymaps done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<YMaps>
+		<YMaps query={{ apikey: import.meta.env.VITE_YMAPS_API_KEY }}>
 			<App />
 		</YMaps>
 	</StrictMode>
