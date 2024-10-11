aba481dfff9fd448e196680e1657e2b517df0f1c
Ryu <rastsislauyushkevich@gmail.com>
2024-10-11 11:34:33 +0300

    gmaps ready

--- frontend/src/components/GoogleMap.jsx
+++ frontend/src/components/GoogleMap.jsx
@@ -1,22 +1,24 @@
-import { Map, useMapsLibrary } from '@vis.gl/react-google-maps';
+import { AdvancedMarker, Map } from '@vis.gl/react-google-maps';
 import { useGetLocation } from '../hooks/useGetLocation';
 import { useEffect, useState } from 'react';
 import { usePlacesService } from '../hooks/usePlacesService';
 
 const GoogleMap = () => {
 	const { location, isLoading } = useGetLocation();
-	// const [places, setPlaces] = useState([]);
-	// const placesService = usePlacesService();
+	const [places, setPlaces] = useState([]);
+	const placesService = usePlacesService();
 
-	// console.log(placesService);
+	console.log(placesService);
 
-	// useEffect(() => {
-	// 	placesService.nearbySearch(
-	// 		{ location, radius: '5000', keyword: 'A100' },
-	// 		(res) => setPlaces(res)
-	// 	);
-	// }, [placesService]);
-
+	useEffect(() => {
+		if (placesService) {
+			placesService.nearbySearch(
+				{ location, radius: '5000', keyword: 'A100' },
+				(res) => setPlaces(res)
+			);
+		}
+	}, [placesService]);
+	console.log(places);
 	return (
 		<div>
 			{!isLoading && location.lat && (
@@ -26,7 +28,18 @@ const GoogleMap = () => {
 					defaultZoom={20}
 					gestureHandling={'greedy'}
 					disableDefaultUI={true}
-				/>
+					mapId={'cbd1b6c3063f1260'}
+				>
+					{places.map((el, index) => (
+						<AdvancedMarker
+							key={index}
+							position={{
+								lat: el.geometry.location.lat(),
+								lng: el.geometry.location.lng(),
+							}}
+						/>
+					))}
+				</Map>
 			)}
 		</div>
 	);
--- frontend/src/hooks/usePlacesService.js
+++ frontend/src/hooks/usePlacesService.js
@@ -1,13 +1,18 @@
 import { useState, useEffect } from 'react';
-import { useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
+import {
+	useApiIsLoaded,
+	useMap,
+	useMapsLibrary,
+} from '@vis.gl/react-google-maps';
 
 export const usePlacesService = () => {
 	const map = useMap();
 	const placesLibrary = useMapsLibrary('places');
+	const apiIsLoaded = useApiIsLoaded();
 	const [placesService, setPlacesService] = useState(null);
 
 	useEffect(() => {
-		if (placesLibrary && map) {
+		if (placesLibrary && map && apiIsLoaded) {
 			setPlacesService(new placesLibrary.PlacesService(map));
 		}
 	}, [placesLibrary, map]);
